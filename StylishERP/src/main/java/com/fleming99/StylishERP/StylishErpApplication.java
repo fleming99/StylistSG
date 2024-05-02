3d626a8e6732372f1391bf2731630069b8947958
Rafael Fleming <rfleming1235@gmail.com>
2024-05-02 17:39:42 -0300

    Add files via upload
    Initial project.

---
+++ StylishERP/src/main/java/com/fleming99/StylishERP/StylishErpApplication.java
@@ -0,0 +1,13 @@
+package com.fleming99.StylishERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylishErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StylishErpApplication.class, args);
+	}
+
+}
---
+++ StylishERP/src/test/java/com/fleming99/StylishERP/StylishErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleming99.StylishERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylishErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
